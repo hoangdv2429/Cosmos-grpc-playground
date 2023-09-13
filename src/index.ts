46bbc64a3a13e7d67a59815cc4ac5709242dc7eb
hoangdv2429 <hoangdv2429@gmail.com>
2023-09-13 18:37:58 +0700

    yarn and evmos signing

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,126 @@
 import { chains } from "chain-registry";
 import { getOfflineSignerProto as getOfflineSigner } from "cosmjs-utils";
 import { evmos, osmosis, ibc } from "./codegen_grpc_gateway";
-import {
-  getSigningCosmosClient,
-  getSigningIbcClient,
-} from "./codegen_grpc_gateway";
-import { TxRaw } from "./codegen_grpc_gateway/cosmos/tx/v1beta1/tx";
+import { Int53, Uint53, Decimal } from "@cosmjs/math";
 import { BroadcastMode } from "./codegen_grpc_gateway/cosmos/tx/v1beta1/service";
-import {
-  AuthorizationType,
-  StakeAuthorization,
-} from "./codegen_grpc_gateway/cosmos/staking/v1beta1/authz";
 import Long from "long";
-import { Member } from "./codegen_grpc_gateway/cosmos/group/v1/types";
+//----
 import {
-  MsgCreateGroupWithPolicy,
-  MsgSubmitProposal,
-} from "./codegen_grpc_gateway/cosmos/group/v1/tx";
+  broadcast,
+  getSender,
+  signTransaction,
+} from "@hanchon/evmos-ts-wallet";
+import {
+  SigningStargateClient,
+  StdFee,
+  accountFromAny,
+} from "@cosmjs/stargate";
+import { PubKey } from "./codegen_grpc_gateway/cosmos/crypto/secp256k1/keys";
+import { Any } from "./codegen_grpc_gateway/google/protobuf/any";
+import { decodeEthermintAccount } from "@evmos/proto";
+import { TxRaw } from "./codegen_grpc_gateway/cosmos/tx/v1beta1/tx";
+import {
+  OfflineSigner,
+  makeAuthInfoBytes,
+  makeSignDoc,
+} from "@cosmjs/proto-signing";
+import { fromBase64 } from "@cosmjs/encoding";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 
 //transaction transition is sign => encode => broadcast
 
-const main = async () => {
-  const _address = "evmos1yqmz73kfd0ap3g9d50uw7y82sl5k8fs0a3xfvn";
+async function sign(
+  telescopeClient: any,
+  client: SigningStargateClient, // SigningStargateClient
+  signer: DirectSecp256k1HdWallet, // keplr OfflineSigner
+  chainId: string,
+  signerAddress: string,
+  messages: any[],
+  fee: StdFee,
+  memo: string
+) {
+  // Query account info, because cosmjs doesn't support Evmos account
+  // GET /cosmos/auth/v1beta1/accounts/{address}
+  const { accountNumber, sequence } =
+    await telescopeClient.cosmos.auth.v1beta1.account({
+      address: signerAddress,
+    });
+  const accountFromSigner = (await signer.getAccounts()).find(
+    (account) => account.address === signerAddress
+  );
+  if (!accountFromSigner) {
+    throw new Error("Failed to retrieve account from signer");
+  }
+  const pubkeyBytes = accountFromSigner.pubkey;
 
-  //create gRPC-web client
+  // Custom typeUrl for EVMOS
+  const pubk = Any.fromPartial({
+    typeUrl: "/ethermint.crypto.v1.ethsecp256k1.PubKey",
+    value: PubKey.encode({
+      key: pubkeyBytes,
+    }).finish(),
+  });
+
+  const txBodyEncodeObject = {
+    typeUrl: "/cosmos.tx.v1beta1.TxBody",
+    value: {
+      messages: messages,
+      memo: memo,
+    },
+  };
+  const txBodyBytes = client.registry.encode(txBodyEncodeObject);
+  const gasLimit = Int53.fromString(fee.gas).toNumber();
+  const authInfoBytes = makeAuthInfoBytes(
+    [{ pubkey: pubk, sequence }],
+    fee.amount,
+    gasLimit,
+    signerAddress,
+    signerAddress
+  );
+  const signDoc = makeSignDoc(
+    txBodyBytes,
+    authInfoBytes,
+    chainId,
+    accountNumber
+  );
+  const { signature, signed } = await signer.signDirect(signerAddress, signDoc);
+
+  // returns txBytes for broadcast
+  return TxRaw.encode({
+    bodyBytes: signed.bodyBytes,
+    authInfoBytes: signed.authInfoBytes,
+    signatures: [fromBase64(signature.signature)],
+  }).finish();
+}
+
+const main = async () => {
+  const mnemonic = "";
+  const _address = "realio1edc9dwhzp9qzq58f0cslzuqlygeauntcfe4tmp";
+
+  //create client
+  const endpoint = "https://realio-api.genznodes.dev";
+  const rpcendpoint = "https://realio-rpc.genznodes.dev";
   const client = await evmos.ClientFactory.createGrpcGateWayClient({
-    // rpcEndpoint: 'https://grpc.testnet.secretsaturn.net'
-    // rpcEndpoint: 'https://juno-grpc-web.polkachu.com/'
-    // ✨  Done in 28.57s. This is grpc-web
-    // rpcEndpoint: 'https://osmosis-grpc-web.polkachu.com/'
-    // ✨  Done in 43.94s. This is grpc-gateway
-    // rpcEndpoint: 'https://osmosis-rpc.polkachu.com'
-    // Osmosis mainnet
-    // endpoint: 'https://lcd.osmosis.zone'
-    // Osmosis testnet
-    endpoint: "https://evmos-testnet-api.polkachu.com",
+    endpoint: endpoint,
   });
 
   // this part is for testing connection
-  // console.time("node_status");
   // const nodeStatus = await client.cosmos.base.tendermint.v1beta1.getNodeInfo(
   //   {}
   // );
   // console.log(nodeStatus);
   // return;
 
-  // console.timeEnd("node_status");
+  const accountParser = (account: any) => {
+    try {
+      return decodeEthermintAccount(account);
+    } catch {
+      return accountFromAny(account);
+    }
+  };
+
   // get signer data
   let account;
-  console.time("accountInfo");
   try {
     account = await client.cosmos.auth.v1beta1.account({
       address: _address,
@@ -56,43 +128,25 @@ const main = async () => {
   } catch (error) {
     console.log(error);
   }
-  console.timeEnd("accountInfo");
-  console.log(account);
 
-  let account_eth;
-  console.time("account_eth_Info");
-  try {
-    account = await client.evmos.({
-      address: _address,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-  console.timeEnd("account_eth_Info");
-  console.log(account);
-
-  return;
-
-  const baseAccount =
-    account.account as import("./codegen_grpc_gateway/cosmos/auth/v1beta1/auth").BaseAccount;
+  const baseAccount = account.account;
+  console.log(baseAccount);
 
   let signerData;
   try {
     signerData = {
-      accountNumber: Number(baseAccount.account_number),
-      sequence: Number(baseAccount.sequence),
-      //chainId: 'pulsar-2',
-      //chainId: 'osmosis-1'
-      chainId: "centauri-1",
+      accountNumber: Number(baseAccount.base_account?.account_number),
+      sequence: Number(baseAccount.base_account?.sequence),
+      chainId: "realionetwork_3301-1",
     };
+    console.log("signer data: ", JSON.stringify(signerData));
   } catch (error) {
     console.log("error getting signer data!!:", JSON.stringify(signerData));
     return;
   }
 
   //for whomever take my 1 OSMO when I'm developing for the whole community, I pity you
-  const mnemonic = "";
-  const chain = chains.find(({ chain_name }) => chain_name === "composable");
+  const chain = chains.find(({ chain_name }) => chain_name === "realio");
   if (!chain) {
     console.log("check chain name!");
   }
@@ -103,51 +157,50 @@ const main = async () => {
     chain,
   });
 
-  //get signer
-  const signClient = await getSigningIbcClient({
-    rpcEndpoint: "https://cosmos-rpc.quickapi.com:443",
-    signer,
-  });
+  // Initialize the StargateClient with this registry.
+  // const options = { accountParser: accountParser };
+  const options = {
+    accountParser: accountParser,
+    gasPrice: { amount: Decimal.fromUserInput("2500000", 0), denom: "ario" },
+  };
+
+  let SGClient: SigningStargateClient;
+  try {
+    SGClient = await SigningStargateClient.connectWithSigner(
+      rpcendpoint,
+      signer,
+      {
+        gasPrice: {
+          amount: Decimal.fromUserInput("2500000", 0),
+          denom: "ario",
+        },
+        prefix: "realio",
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log(await signer.getAccounts());
 
   //--------------------------------------------
-  // example for send msg
+  // example for ibc-transfer msg
   //--------------------------------------------
 
-  // const { send } = cosmos.bank.v1beta1.MessageComposer.withTypeUrl;
-  // const msg = send({
-  //   amount: [
-  //     {
-  //       denom: "uosmo",
-  //       amount: "1000",
-  //     },
-  //   ],
-  //   toAddress: _address,
-  //   fromAddress: _address,
-  // });
-
-  // const fee = {
-  //   amount: [
-  //     {
-  //       denom: "uosmo",
-  //       amount: "864",
-  //     },
-  //   ],
-  //   gas: "86364",
-  // };
   const stamp = Date.now();
   const timeoutInNanos = (stamp + 1.2e6) * 1e6;
   const { transfer } = ibc.applications.transfer.v1.MessageComposer.withTypeUrl;
 
   const msg = transfer({
     sourcePort: "transfer",
-    sourceChannel: "channel-184",
+    sourceChannel: "channel-1",
     token: {
-      denom:
-        "ibc/7078D8FA8BCB50A1E7855458F1E9898A3FDA0093B703DA3B3108DD35ED99E048",
-      amount: "46500000",
+      denom: "ario",
+      amount: "98551685000000000",
     },
-    sender: "centauri19crd4fwzm9qtf5ln5l3e2vmquhevjwprjdx5zj",
-    receiver: "stars147auavf4tvghskslq2w65de0nh5dqdmlx60rax",
+    sender: _address,
+    receiver: "osmo19crd4fwzm9qtf5ln5l3e2vmquhevjwpr7uccsn",
     timeoutHeight: {
       revisionNumber: Long.fromString("0"),
       revisionHeight: Long.fromString("0"),
@@ -159,26 +212,26 @@ const main = async () => {
   const fee = {
     amount: [
       {
-        denom:
-          "ibc/7078D8FA8BCB50A1E7855458F1E9898A3FDA0093B703DA3B3108DD35ED99E048",
-        amount: "250000",
+        denom: "ario",
+        amount: "2500000",
       },
     ],
-    gas: "250000",
+    gas: "2500000",
   };
 
-  const account_data = await signer.getAccounts();
-  // console.log('account_data: ', account_data);
-
-  const signed_tx = await signClient.sign(
+  const txRawBytes = await sign(
+    client,
+    SGClient,
+    await DirectSecp256k1HdWallet.fromMnemonic(mnemonic),
+    signerData.chainId,
     _address,
     [msg],
     fee,
-    "grpc gateway",
-    signerData
+    "repay"
   );
-  // console.log(signed_tx);
-  const txRawBytes = Uint8Array.from(TxRaw.encode(signed_tx).finish());
+  if (!txRawBytes) {
+    console.log("error signing transaction");
+  }
 
   // uncomment the following snippet to send transaction
   console.time("broadcastTx");
@@ -195,165 +248,6 @@ const main = async () => {
   console.timeEnd("broadcastTx");
 
   // console.log(res);
-
-  //--------------------------------------------
-  // example for authz
-  //--------------------------------------------
-
-  // const fee = {
-  //   amount: [
-  //     {
-  //       denom: "uosmo",
-  //       amount: "1864",
-  //     },
-  //   ],
-  //   gas: "186364",
-  // };
-
-  // const account_data = await signer.getAccounts();
-  // console.log(account_data);
-
-  // const { grant } = cosmos.authz.v1beta1.MessageComposer.withTypeUrl;
-  // const msg = grant({
-  //   granter: _address,
-  //   grantee: "osmo19crd4fwzm9qtf5ln5l3e2vmquhevjwpr7uccsn",
-  //   grant: {
-  //     authorization: StakeAuthorization.toProtoMsg({
-  //       maxTokens: {
-  //         denom: "uosmo",
-  //         amount: "100000000",
-  //       },
-  //       authorizationType: AuthorizationType.AUTHORIZATION_TYPE_DELEGATE,
-  //     }),
-  //     expiration: new Date(Date.now() + 60 * 60 * 24 * 7),
-  //   },
-  // });
-
-  // const msgs = [];
-  // msgs.push(msg);
-
-  // const { send } = cosmos.bank.v1beta1.MessageComposer.withTypeUrl;
-  // const msg1 = send({
-  //   amount: [
-  //     {
-  //       denom: "uosmo",
-  //       amount: "1000",
-  //     },
-  //   ],
-  //   toAddress: _address,
-  //   fromAddress: _address,
-  // });
-
-  // msgs.push(msg1);
-
-  // // propose change
-  // // const res = client.cosmos.authz.v1beta1.grant.simulate(msg, signer);
-  // const signed_tx = await signClient.sign(
-  //   _address,
-  //   msgs,
-  //   fee,
-  //   "Diff",
-  //   signerData
-  // );
-  // console.log(signed_tx);
-  // const txRawBytes = Uint8Array.from(TxRaw.encode(signed_tx).finish());
-
-  // const res = await client.cosmos.tx.v1beta1.broadcastTx({
-  //   txBytes: txRawBytes,
-  //   mode: BroadcastMode.BROADCAST_MODE_BLOCK,
-  // });
-
-  // console.log(res);
-
-  //--------------------------------------------
-  // createGroupWithPolicy example
-  //--------------------------------------------
-
-  // const {
-  //     createGroupWithPolicy,
-  // } = cosmos.group.v1.MessageComposer.withTypeUrl;
-
-  // const {
-  //     PercentageDecisionPolicy
-  // } = cosmos.group.v1;
-
-  //   const fee = {
-  //     amount: [
-  //         {
-  //             denom: 'uosmo',
-  //             amount: '864'
-  //         }
-  //         ],
-  //         gas: '86364'
-  // }
-
-  // const msg = createGroupWithPolicy({
-  //     admin: _address,
-  //     groupMetadata: 'testing group metadata',
-  //     groupPolicyAsAdmin: true,
-  //     groupPolicyMetadata: 'testing group policy metadata',
-  //     members: [{
-  //         address: _address,
-  //         weight: '50',
-  //         metadata: 'test-0',
-  //         addedAt: new Date(Date.now()),
-  //     } ],
-  //     decisionPolicy: PercentageDecisionPolicy.toProtoMsg({
-  //         percentage: '0.5',
-  //         windows: {
-  //             votingPeriod: {
-  //                 nanos: 0,
-  //                 seconds: Long.fromValue(3600)
-  //             },
-  //             minExecutionPeriod: {
-  //                 nanos: 0,
-  //                 seconds: Long.fromValue(3600)
-  //             }
-  //     }
-  //     })});
-
-  // // const policy: MsgCreateGroupWithPolicyEncoded = {
-  // //     admin: _address,
-  // //     groupMetadata: 'testing group metadata',
-  // //     groupPolicyAsAdmin: true,
-  // //     groupPolicyMetadata: 'testing group policy metadata',
-  // //     members: [{
-  // //         address: _address,
-  // //         weight: '50',
-  // //         metadata: 'test-0',
-  // //         addedAt: new Date(Date.now()),
-  // //     } ],
-  // //     decisionPolicy: PercentageDecisionPolicy.toProtoMsg({
-  // //         percentage: '0.5',
-  // //         windows: {
-  // //             votingPeriod: {
-  // //                 nanos: 0,
-  // //                 seconds: Long.fromValue(3600)
-  // //             },
-  // //             minExecutionPeriod: {
-  // //                 nanos: 0,
-  // //                 seconds: Long.fromValue(3600)
-  // //             }
-  // //     }
-  // //     })};
-
-  // //     const msg = {
-  // //         typeUrl: '/cosmos.group.v1.MsgCreateGroupWithPolicy',
-  // //         value: policy
-  // //     }
-
-  //     const signed_tx = await signClient.sign(_address, [msg], fee, 'grpc-gateway',signerData);
-  //     console.log(signed_tx);
-  //     const txRawBytes = Uint8Array.from(TxRaw.encode(signed_tx).finish());
-
-  //     const res = await client.cosmos.tx.v1beta1.broadcastTx(
-  //       {
-  //         txBytes: txRawBytes,
-  //         mode: BroadcastMode.BROADCAST_MODE_BLOCK
-  //       }
-  //     )
-
-  //     console.log(res);
 };
 
 main().then(() => {
